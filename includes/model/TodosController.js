8ca16181dffab58424326d7257a14a838882ae84
Raúl Torres Alarcón <raultorres2603@gmail.com>
2022-11-06 17:16:13 +0100

    MenuController function

--- includes/model/TodosController.js
+++ includes/model/TodosController.js
@@ -9,6 +9,7 @@ class TodosController {
   #todoInsertInp;
   #tableTODOSbody;
   #init;
+  #menu;
 
   constructor() {
     this.#init = this.#initController();
@@ -30,7 +31,9 @@ class TodosController {
    * Function that is called when the Controller it's created
    */
   #initController() {
+    this.setMenu("main");
     window.addEventListener("load", (ev) => {
+      this.#menuController();
       if (localStorage.getItem("todos")) {
         this.setTodos(this.convertLocalStorage());
       }
@@ -104,6 +107,79 @@ class TodosController {
     });
   }
 
+  #menuController() {
+    switch (this.getMenu()) {
+      case "main":
+        document.body.innerHTML = `<div class="row">
+      <div class="col-md-12">
+        <div class="display-2 text-center">
+          TODO's
+        </div>
+        <div class="row">
+          <div class="col-md-6">
+            <div class="card">
+              <h5 class="card-header display-4 text-center bg-info">List</h5>
+              <div class="card-body">
+                <div class="row">
+                  <table class="table tableTODOS">
+                    <thead class="table-dark text-center">
+                      <tr>
+                        <th>TODO</th>
+                        <th>Creation</th>
+                        <th>Finalization</th>
+                        <th>Delete</th>
+                      </tr>
+                    </thead>
+                    <tbody class="text-center tBodyTODO">
+                    </tbody>
+                  </table>
+                </div>
+  
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="card">
+              <h5 class="card-header display-4 text-center bg-info">Create</h5>
+              <div class="card-body">
+                <div class="row">
+                  <div class="col-md-6">
+                    <div class="input-group mb-3">
+                      <div class="input-group-prepend">
+                        <span class="input-group-text" id="basic-addon1">TODO</span>
+                      </div>
+                      <input type="text" class="form-control" id="todoInp" placeholder="Ex: Do homework" aria-label="homework" aria-describedby="basic-addon1">
+                    </div>
+                  </div>
+                  <div class="col-md-6">
+                    <div class="input-group mb-3">
+                      <div class="input-group-prepend">
+                        <span class="input-group-text" id="basic-addon1">Finalization</span>
+                      </div>
+                      <input type="date" class="form-control" id="todoFinInp" aria-label="homework" aria-describedby="basic-addon1">
+                    </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-md-12">
+                    <div class="d-grid gap-2">
+                      <button type="button" id="insertTodo" class="btn btn-success btn-lg btn-block">Insert</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>`;
+        break;
+
+      default:
+        break;
+    }
+  }
+
   /////////////////////////////// SETTERS AND GETTERS ///////////////////////////////
 
   /**
@@ -147,10 +223,25 @@ class TodosController {
   }
 
   /**
+   *  Getter of menu
+   * @returns {HTMLElement}
+   */
+  getMenu() {
+    return this.#menu;
+  }
+
+  /**
    *  Setter of todoInsertInp
    */
   setTodoInsert(todoInsInp) {
     this.#todoInsertInp = todoInsInp;
+  }
+
+  /**
+   *  Setter of menu
+   */
+  setMenu(menu) {
+    this.#menu = menu;
   }
 
   /**
