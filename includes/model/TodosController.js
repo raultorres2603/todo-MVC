9f5c098d0b45eca001c2ed6f4e0589aa282a26a8
Raul Torres <55553058+raultorres2603@users.noreply.github.com>
2022-11-09 19:24:13 +0100

    Modulated

--- includes/model/TodosController.js
+++ includes/model/TodosController.js
@@ -1,4 +1,5 @@
 import { Todo } from "./Todo.js";
+import { View } from "./View.js";
 
 /**
  * Represents a Controller of the APP
@@ -33,8 +34,8 @@ export class TodosController {
    * Function that is called when the Controller it's created
    */
   #initController() {
-    this.setMenu("main");
     window.addEventListener("load", (ev) => {
+      this.setMenu("main");
       if (localStorage.getItem("todos")) {
         this.setTodos(this.convertLocalStorage());
       }
@@ -68,6 +69,42 @@ export class TodosController {
   }
 
   /**
+   * Draw method on table body
+   */
+  #draw() {
+    switch (this.getMenu()) {
+      case "main":
+        let tableBody = this.getTodoTableBody();
+        // Clean table body
+        tableBody.replaceChildren();
+        // For each todo, insert row with the cells
+        this.getTodos().forEach((todo, index) => {
+          //Creamos la ROW para meter las celdas siguientes
+          let row = tableBody.insertRow();
+          let todoName = row.insertCell();
+          todoName.innerHTML = todo.getName();
+          let todoCreate = row.insertCell();
+          todoCreate.innerHTML = new Date(
+            todo.getCreated()
+          ).toLocaleDateString();
+          let todoFinal = row.insertCell();
+          todoFinal.innerHTML = new Date(todo.getFinal()).toLocaleDateString();
+          let todoDelete = row.insertCell();
+          todoDelete.innerHTML = `<div class="d-grid gap-2">
+      <button class="btn btn-danger align-middle" type="button">Delete</button>
+    </div>`;
+          todoDelete.addEventListener("click", (ev) => {
+            this.#deleteTodo(index);
+          });
+        });
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  /**
    * Function that adds a new Todo to the prop and localStorage
    * @param {Todo} todo
    */
@@ -87,105 +124,9 @@ export class TodosController {
     this.#draw();
   }
 
-  /**
-   * Draw method on table body
-   */
-  #draw() {
-    let tableBody = this.getTodoTableBody();
-    // Clean table body
-    tableBody.replaceChildren();
-    // For each todo, insert row with the cells
-    this.getTodos().forEach((todo, index) => {
-      //Creamos la ROW para meter las celdas siguientes
-      let row = tableBody.insertRow();
-      let todoName = row.insertCell();
-      todoName.innerHTML = todo.getName();
-      let todoCreate = row.insertCell();
-      todoCreate.innerHTML = new Date(todo.getCreated()).toLocaleDateString();
-      let todoFinal = row.insertCell();
-      todoFinal.innerHTML = new Date(todo.getFinal()).toLocaleDateString();
-      let todoDelete = row.insertCell();
-      todoDelete.innerHTML = `<div class="d-grid gap-2">
-      <button class="btn btn-danger align-middle" type="button">Delete</button>
-    </div>`;
-      todoDelete.addEventListener("click", (ev) => {
-        this.#deleteTodo(index);
-      });
-    });
-  }
-
   #menuController() {
-    switch (this.getMenu()) {
-      case "main":
-        document.querySelector(".root").innerHTML = `<div class="row">
-      <div class="col-md-12">
-        <div class="display-2 text-center">
-          TODO's
-        </div>
-        <div class="row">
-          <div class="col-md-6">
-            <div class="card">
-              <h5 class="card-header display-4 text-center bg-info">List</h5>
-              <div class="card-body">
-                <div class="row">
-                  <table class="table tableTODOS">
-                    <thead class="table-dark text-center">
-                      <tr>
-                        <th>TODO</th>
-                        <th>Creation</th>
-                        <th>Finalization</th>
-                        <th>Delete</th>
-                      </tr>
-                    </thead>
-                    <tbody class="text-center tBodyTODO">
-                    </tbody>
-                  </table>
-                </div>
-  
-              </div>
-            </div>
-          </div>
-          <div class="col-md-6">
-            <div class="card">
-              <h5 class="card-header display-4 text-center bg-info">Create</h5>
-              <div class="card-body">
-                <div class="row">
-                  <div class="col-md-6">
-                    <div class="input-group mb-3">
-                      <div class="input-group-prepend">
-                        <span class="input-group-text" id="basic-addon1">TODO</span>
-                      </div>
-                      <input type="text" class="form-control" id="todoInp" placeholder="Ex: Do homework" aria-label="homework" aria-describedby="basic-addon1">
-                    </div>
-                  </div>
-                  <div class="col-md-6">
-                    <div class="input-group mb-3">
-                      <div class="input-group-prepend">
-                        <span class="input-group-text" id="basic-addon1">Finalization</span>
-                      </div>
-                      <input type="date" class="form-control" id="todoFinInp" aria-label="homework" aria-describedby="basic-addon1">
-                    </div>
-                  </div>
-                </div>
-                <div class="row">
-                  <div class="col-md-12">
-                    <div class="d-grid gap-2">
-                      <button type="button" id="insertTodo" class="btn btn-success btn-lg btn-block">Insert</button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>`;
-        this.#addListeners();
-        break;
-
-      default:
-        break;
-    }
+    View.render(this.getMenu());
+    this.#addListeners();
   }
 
   /////////////////////////////// SETTERS AND GETTERS ///////////////////////////////
